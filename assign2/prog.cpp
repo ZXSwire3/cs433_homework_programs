343effd762823fde05cf79bdcb13e46aeb2e16eb
benfoltz <eggie.foltz@gmail.com>
2024-02-29 12:53:45 -0800

    passed pipe test

--- assign2/prog.cpp
+++ assign2/prog.cpp
@@ -38,25 +38,25 @@ int parse_command(char command[], char *args[]) {
         return -1;
     }
 
-    int i = 0; // Index of the argument
+    int i = 0;  // Index of the argument
     while (token != nullptr) {
         token[strcspn(token, "\n")] = '\0';  // Remove newline character from the token
 
         // Allocate memory for the argument and copy the token into it
         try {
             args[i] = new char[strlen(token) + 1];
-        } catch (const std::bad_alloc& e) {
+        } catch (const std::bad_alloc &e) {
             cout << "Memory allocation failed: " << e.what() << endl;
             return -1;
         }
-        strcpy(args[i], token); // Copy the token into the argument array
+        strcpy(args[i], token);  // Copy the token into the argument array
 
         i++;
-        token = strtok(nullptr, " "); // Get the next argument
+        token = strtok(nullptr, " ");  // Get the next argument
     }
     args[i] = nullptr;  // Set the last argument to NULL
 
-    return i; // Return the number of arguments
+    return i;  // Return the number of arguments
 }
 
 /**
@@ -69,11 +69,19 @@ string argsToString(char *args[]) {
     string str;
     for (int i = 0; args[i] != nullptr; i++) {
         if (i != 0) {
-            str += " "; // Add a space between arguments
+            str += " ";  // Add a space between arguments
         }
-        str += args[i]; // Add the argument to the string
+        str += args[i];  // Add the argument to the string
     }
     return str;
+}
+
+int executeCommand(char *args[]) {
+    if (execvp(args[0], args) == -1) {
+        cout << "Command not found" << endl;
+        return -1;
+    }
+    return 0;
 }
 
 /**
@@ -106,10 +114,10 @@ int main(int argc, char *argv[]) {
          * (3) parent will invoke wait() unless command included &
          */
 
-        if (num_args == -1) {
+        if (num_args == -1) {  // If no command was provided
             cout << "No command" << endl;
             continue;
-        } else if (num_args > 0) {
+        } else if (num_args > 0) {  // If there are arguments
             // Check if the command is "exit"
             if (strcmp(args[0], "exit") == 0) {
                 cout << "Exiting..." << endl;
@@ -148,62 +156,117 @@ int main(int argc, char *argv[]) {
             }
 
             // Check for background execution (contains ampersand)
-            if (strcmp(args[num_args - 1], "&") == 0) { // if last argument is &
+            if (strcmp(args[num_args - 1], "&") == 0) {  // if last argument is &
                 contains_ampersand = true;
-                args[num_args - 1] = nullptr; // Remove the ampersand from the arguments list
-                num_args--; // Decrement the number of arguments
-            } else if (args[num_args - 1][strlen(args[num_args - 1]) - 1] == '&') { // if last character of last argument is &
+                args[num_args - 1] = nullptr;  // Remove the ampersand from the arguments list
+                num_args--;  // Decrement the number of arguments
+            } else if (args[num_args - 1][strlen(args[num_args - 1]) - 1] ==
+                       '&') {  // if last character of last argument is &
                 contains_ampersand = true;
-                args[num_args - 1][strlen(args[num_args - 1]) - 1] = '\0'; // Remove the ampersand from the arguments list
+                args[num_args - 1][strlen(args[num_args - 1]) - 1] =
+                        '\0';  // Remove the ampersand from the arguments list
             } else {
                 contains_ampersand = false;
             }
 
+            // Fork a child process
             pid_t pid = fork();
             if (pid < 0) {
                 cout << "Fork failed" << endl;
                 return 1;
-            } else if (pid == 0) {
-//                cout << "Child process" << endl;
-
+            } else if (pid == 0) {  // Child process
                 // Loop through the arguments
                 for (int i = 0; args[i] != nullptr; i++) {
-
                     // Check for output redirection
                     if (strcmp(args[i], ">") == 0) {
-                        args[i] = nullptr; // Remove the operator from the arguments list
+                        args[i] = nullptr;  // Remove the operator from the arguments list
 
                         // Open the file for writing
                         int fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                        dup2(fd, 1); // Redirect stdout to the file
-                        close(fd); // Close the original file descriptor
+                        dup2(fd, 1);  // Redirect stdout to the file
+                        close(fd);  // Close the original file descriptor
 
-                        args[i + 1] = nullptr; // Remove the file name from the arguments list
+                        args[i + 1] = nullptr;  // Remove the file name from the arguments list
                         break;
                     }
 
                     // Check for input redirection
                     if (strcmp(args[i], "<") == 0) {
-                        args[i] = nullptr; // Remove the operator from the arguments list
+                        args[i] = nullptr;  // Remove the operator from the arguments list
 
                         // Open the file for reading
                         int fd = open(args[i + 1], O_RDONLY);
-                        dup2(fd, STDIN_FILENO); // Redirect stdin to the file
-                        close(fd); // Close the original file descriptor
+                        dup2(fd, STDIN_FILENO);  // Redirect stdin to the file
+                        close(fd);  // Close the original file descriptor
 
-                        args[i + 1] = nullptr; // Remove the file name from the arguments list
+                        args[i + 1] = nullptr;  // Remove the file name from the arguments list
                         break;
                     }
 
+                    // Check for pipe
+                    if (strcmp(args[i], "|") == 0) {
+                        args[i] = nullptr;  // Remove the pipe operator from the arguments list
+
+                        // Create a pipe
+                        int pipefd[2];
+                        if (pipe(pipefd) == -1) {
+                            cout << "Pipe failed" << endl;
+                            return 1;
+                        }
+
+                        // Fork a child process for the first command
+                        pid_t pid1 = fork();
+                        if (pid1 < 0) {
+                            cout << "Fork failed" << endl;
+                            return 1;
+                        } else if (pid1 == 0) {
+                            // In the child process, close the read end of the pipe
+                            close(pipefd[0]);
+
+                            // Redirect stdout to the write end of the pipe
+                            dup2(pipefd[1], STDOUT_FILENO);
+                            close(pipefd[1]);
+
+                            // Execute the first command
+                            executeCommand(args);
+                        }
+
+                        // Prepare the arguments for the second command
+                        char **args2 = &args[i + 1];
+
+                        // Fork another child process for the second command
+                        pid_t pid2 = fork();
+                        if (pid2 < 0) {
+                            perror("fork");
+                            exit(EXIT_FAILURE);
+                        } else if (pid2 == 0) {
+                            // In the child process, close the write end of the pipe
+                            close(pipefd[1]);
+
+                            // Redirect stdin to the read end of the pipe
+                            dup2(pipefd[0], STDIN_FILENO);
+                            close(pipefd[0]);
+
+                            // Execute the second command
+                            executeCommand(args2);
+                        }
+
+                        // In the parent process, close both ends of the pipe
+                        close(pipefd[0]);
+                        close(pipefd[1]);
+
+                        // Wait for both child processes to finish
+                        waitpid(pid1, nullptr, 0);
+                        waitpid(pid2, nullptr, 0);
+
+                        // Break the loop as we've found and processed the pipe
+                        break;
+                    }
                 }
 
-                if (execvp(args[0], args) == -1) {
-                    cout << "Command not found" << endl;
-                    return 1;
-                }
-            } else {
-//                cout << "Parent process" << endl;
-
+                // Execute the command
+                executeCommand(args);
+            } else {  // Parent process
                 if (!contains_ampersand) {
                     waitpid(pid, nullptr, 0);
                 } else {
