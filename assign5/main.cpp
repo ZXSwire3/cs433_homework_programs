80e8efb01abce1ba99d738540e4f15bdd2be8535
benfoltz <eggie.foltz@gmail.com>
2024-05-02 12:53:05 -0700

    fixed print statements and added header

--- assign5/main.cpp
+++ assign5/main.cpp
@@ -1,11 +1,17 @@
-// Remember to add comments to your code
+/**
+ * Assignment 5: Page replacement algorithms
+ * @file main.cpp
+ * @author Ben Foltz-Miranda
+ * @brief The main program to test page replacement algorithms
+ * @version 0.1
+ */
 
+#include <chrono>
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <chrono>
 
 #include "fifo_replacement.h"
 #include "lifo_replacement.h"
@@ -21,8 +27,8 @@ int main(int argc, char *argv[]) {
     // Print basic information about the program
     std::cout << "=================================================================" << std::endl;
     std::cout << "CS 433 Programming assignment 5" << std::endl;
-    std::cout << "Author: xxxxxx and xxxxxxx" << std::endl;
-    std::cout << "Date: xx/xx/20xx" << std::endl;
+    std::cout << "Author: Ben Foltz-Miranda" << std::endl;
+    std::cout << "Date: 05/02/2024" << std::endl;
     std::cout << "Course: CS433 (Operating Systems)" << std::endl;
     std::cout << "Description : Program to simulate different page replacement algorithms" << std::endl;
     std::cout << "=================================================================\n" << std::endl;
